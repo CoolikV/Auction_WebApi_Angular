dc9210d6bbfdbd37a4c993e5d12e3bded523b8a5
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-14 20:42:35 +0300

    Described interface for user manager

--- Auction.BusinessLogic/Interfaces/IUserManager.cs
+++ Auction.BusinessLogic/Interfaces/IUserManager.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Auction.BusinessLogic.DataTransfer;
+using Auction.BusinessLogic.IdentityDetails;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Auction.BusinessLogic.Interfaces
 {
-    public interface IUserManager
+    public interface IUserManager : IDisposable
     {
-
+        Task<OperationDetails> Create(UserDTO userDto);
+        IEnumerable<UserDTO> GetUsers();
+        Task<Tuple<ClaimsIdentity, ClaimsIdentity>> FindAsync(string username, string password);
+        UserDTO GetUserByName(string name);
+        Task EditRole(string userId, string newRoleName);
+        IEnumerable<string> GetRoles();
     }
 }
