411d063068c24fe96c476089bd487fd219afd3f6
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-14 20:05:55 +0300

    Added interface for user repository

---
+++ Auction.DataAccess/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Auction.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Auction.DataAccess.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        User GetById(string id);
+        void Delete(string id);
+    }
+}
