5d917ee807789091f2313b5840a79b8bb98e4bdf
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-14 20:06:29 +0300

    Implemented user repository

---
+++ Auction.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using Auction.DataAccess.Interfaces;
+using Auction.DataAccess.EF;
+using Auction.DataAccess.Entities;
+
+namespace Auction.DataAccess.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AuctionContext context) 
+            : base(context) { }
+
+        public void Delete(string id)
+        {
+            User user = dbSet.Find(id);
+            base.Delete(user);
+        }
+
+        public User GetById(string id)
+        {
+            return dbSet.Find(id);
+        }  
+    }
+}
