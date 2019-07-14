b07d985ea78ebe426176cb391bf7f8b2ef27d31e
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-14 19:23:54 +0300

    Added Identity support

--- Auction.DataAccess/EF/AuctionContext.cs
+++ Auction.DataAccess/EF/AuctionContext.cs
@@ -1,11 +1,13 @@
 ﻿using System.Data.Entity;
 using Auction.DataAccess.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Auction.DataAccess.Identity.Entities;
 
 namespace Auction.DataAccess.EF
 {
-    public class AuctionContext : DbContext
+    public class AuctionContext : IdentityDbContext<AppUser>
     {
-        public DbSet<User> Users { get; set; }
+        public DbSet<User> UserProfiles { get; set; }
         public DbSet<TradingLot> TradingLots { get; set; }
         public DbSet<Trade> Trades { get; set; }
         public DbSet<Category> Categories { get; set; }
