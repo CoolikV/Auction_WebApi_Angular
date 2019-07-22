76ee78647174731ab0c5e1d2a0b40b064a95ea1c
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-22 14:55:23 +0300

    Created mapster config for DTO <=> data entities

---
+++ Auction.BusinessLogic/Configs/BLRegister.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mapster;
+using Auction.BusinessLogic.DataTransfer;
+using Auction.DataAccess.Entities;
+
+namespace Auction.BusinessLogic.Configs
+{
+    public class BLRegister : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<CategoryDTO, Category>().TwoWays();
+            config.NewConfig<TradeDTO, Trade>().TwoWays();
+            config.NewConfig<TradingLotDTO, TradingLot>().TwoWays();
+            config.NewConfig<UserDTO, User>().TwoWays();
+        }
+    }
+}
