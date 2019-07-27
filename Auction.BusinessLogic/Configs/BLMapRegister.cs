3cf4eb6487157c60166c3476313cbbb47ff300b6
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-27 23:24:59 +0300

    Rename class

---
+++ Auction.BusinessLogic/Configs/BLMapRegister.cs
@@ -0,0 +1,17 @@
+﻿using Auction.BusinessLogic.DataTransfer;
+using Auction.DataAccess.Entities;
+using Mapster;
+
+namespace Auction.BusinessLogic.Configs
+{
+    public class BLMapRegister : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<CategoryDTO, Category>().TwoWays();
+            config.NewConfig<TradeDTO, Trade>().TwoWays();
+            config.NewConfig<TradingLotDTO, TradingLot>().TwoWays();
+            config.NewConfig<UserDTO, User>().TwoWays();
+        }
+    }
+}
