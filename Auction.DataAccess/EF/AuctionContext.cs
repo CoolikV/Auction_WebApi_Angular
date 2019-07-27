2373e05cc5c9af05ffaeacec70656842509b90ca
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-27 23:11:25 +0300

    Added support for DI

--- Auction.DataAccess/EF/AuctionContext.cs
+++ Auction.DataAccess/EF/AuctionContext.cs
@@ -1,18 +1,18 @@
-﻿using System.Data.Entity;
-using Auction.DataAccess.Entities;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Auction.DataAccess.Entities;
 using Auction.DataAccess.Identity.Entities;
+using Auction.DataAccess.Interfaces;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace Auction.DataAccess.EF
 {
-    public class AuctionContext : IdentityDbContext<AppUser>
+    public class AuctionContext : IdentityDbContext<AppUser>, IDataContext // TODO: fluentapi and initial data
     {
-        public DbSet<User> UserProfiles { get; set; }
-        public DbSet<TradingLot> TradingLots { get; set; }
-        public DbSet<Trade> Trades { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> UserProfiles { get; }
+        public DbSet<TradingLot> TradingLots { get; }
+        public DbSet<Trade> Trades { get; }
+        public DbSet<Category> Categories { get; }
 
         public AuctionContext(string connectionString) : base(connectionString) { }
-
     }
 }
