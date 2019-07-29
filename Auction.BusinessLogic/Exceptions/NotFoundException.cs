28f8dd836a05e2a1c1b694aa41815443425a5e7b
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-30 00:01:48 +0300

    Refactoring

---
+++ Auction.BusinessLogic/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Auction.BusinessLogic.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base("Entity not found")
+        { }
+
+        public NotFoundException(string message) : base(message + " does not exist.")
+        { }
+    }
+}
