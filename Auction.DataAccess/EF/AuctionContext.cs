24011ac3b084514d9f4047ee4e6146c30e7e7f59
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-07-30 15:03:15 +0300

    Seeding test data

--- Auction.DataAccess/EF/AuctionContext.cs
+++ Auction.DataAccess/EF/AuctionContext.cs
@@ -2,16 +2,17 @@
 using Auction.DataAccess.Identity.Entities;
 using Auction.DataAccess.Interfaces;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 using System.Data.Entity;
 
 namespace Auction.DataAccess.EF
 {
-    public class AuctionContext : IdentityDbContext<AppUser>, IDataContext // TODO: fluentapi and initial data
+    public class AuctionContext : IdentityDbContext<AppUser>, IDataContext
     {
-        public DbSet<User> UserProfiles { get; }
-        public DbSet<TradingLot> TradingLots { get; }
-        public DbSet<Trade> Trades { get; }
-        public DbSet<Category> Categories { get; }
+        public DbSet<User> UserProfiles { get; set; }
+        public DbSet<TradingLot> TradingLots { get; set; }
+        public DbSet<Trade> Trades { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         public AuctionContext(string connectionString) : base(connectionString) { }
 
@@ -25,6 +26,61 @@ namespace Auction.DataAccess.EF
             protected override void Seed(AuctionContext context)
             {
                 context.Roles.Add(new IdentityRole("user"));
+
+                context.Categories.Add(new Category() { Id = 1, Name = "Test1" });
+                context.Categories.Add(new Category() { Id = 2, Name = "Test2" });
+                context.SaveChanges();
+
+                context.TradingLots.AddRange(new List<TradingLot>()
+                {
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 1},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2},
+                    new TradingLot(){CategoryId = 2}
+                });
 
                 context.SaveChanges();
             }
