9e9431e00358de1f63b0020ffffc43d92e0539f4
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-08-03 21:22:28 +0300

    Model validation

--- Auction.WebApi/Models/CategoryModel.cs
+++ Auction.WebApi/Models/CategoryModel.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Auction.WebApi.Models
 {
     public class CategoryModel
     {
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[А-Я,/а-я\s?]{4,25}$")]
         public string Name { get; set; }
+
         public ICollection<TradingLotModel> TradingLots { get; set; }
     }
 }
