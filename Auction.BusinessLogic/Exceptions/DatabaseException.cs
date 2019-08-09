12a58d14a03053cbdbfe3d05712d25c8d73fb361
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-08-09 23:11:46 +0300

    Exception from database class

---
+++ Auction.BusinessLogic/Exceptions/DatabaseException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Auction.BusinessLogic.Exceptions
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException(string message) : base(message) { }
+        public DatabaseException() : base() { }
+    }
+}
