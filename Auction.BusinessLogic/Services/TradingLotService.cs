d39dbb3bd2eda8b596e9d11d7b6c81cc4eaa7189
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-08-10 13:46:11 +0300

    Refactoring and error handling

--- Auction.BusinessLogic/Services/TradingLotService.cs
+++ Auction.BusinessLogic/Services/TradingLotService.cs
@@ -2,23 +2,25 @@
 using Auction.BusinessLogic.Exceptions;
 using Auction.BusinessLogic.Interfaces;
 using Auction.DataAccess.Entities;
+using Auction.DataAccess.Entities.Enums;
 using Auction.DataAccess.Interfaces;
 using Mapster;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 namespace Auction.BusinessLogic.Services
 {
     public class TradingLotService : ITradingLotService
     {
         IAdapter Adapter { get; set; }
         IUnitOfWork Database { get; set; }
+        ICategoryService CategoryService { get; set; }
 
-        public TradingLotService(IUnitOfWork uow, IAdapter adapter)
+        public TradingLotService(IUnitOfWork uow, IAdapter adapter, ICategoryService categoryService)
         {
             Database = uow;
             Adapter = adapter;
+            CategoryService = categoryService;
         }
 
         public void Dispose()
@@ -41,31 +43,40 @@ namespace Auction.BusinessLogic.Services
 
         public void EditLot(int lotId, TradingLotDTO lot)
         {
-            if (lot == null)
-                throw new ArgumentNullException(nameof(lot));
+            try
+            {
+                var tradingLot = FindTradingLotById(lotId);
+                if (tradingLot.LotStatus == LotStatus.OnSale)
+                    throw new AuctionException("You can`t change the information about the lot after the start of the bidding");
+                //cant ignore null values in mapster
+                //tradingLot = Adapter.Adapt<TradingLot>(lot);
 
-            var tradingLot = FindTradingLotById(lotId);
+                tradingLot.Name = lot.Name;
+                tradingLot.Description = lot.Description;
+                tradingLot.Img = lot.Img;
+                tradingLot.TradeDuration = lot.TradeDuration;
+                tradingLot.Price = lot.Price;
 
-            //if (tradingLot.IsVerified)
-            //    throw new AuctionException("You can`t change the information about the lot after the start of the bidding");
-            //cant ignore null values in mapster
-            //tradingLot = Adapter.Adapt<TradingLot>(lot);
-
-            tradingLot.Name = lot.Name;
-            tradingLot.Description = lot.Description;
-            tradingLot.Img = lot.Img;
-            tradingLot.TradeDuration = lot.TradeDuration;
-            tradingLot.Price = lot.Price;
-
-            Database.TradingLots.UpdateTradingLot(tradingLot);
+                Database.TradingLots.UpdateTradingLot(tradingLot);
+            }
+            catch(DatabaseException ex)
+            {
+                throw ex;
+            }
             Database.Save();
         }
 
         public void RemoveLotById(int lotId)
         {
-            TradingLot tradingLot = FindTradingLotById(lotId);
-
-            Database.TradingLots.DeleteTradingLotById(tradingLot.Id);
+            try
+            {
+                var lot = FindTradingLotById(lotId);
+                Database.TradingLots.DeleteTradingLotById(lotId);
+            }
+            catch (InvalidOperationException)
+            {
+                throw new DatabaseException();
+            }
             Database.Save();
         }
 
@@ -94,17 +105,17 @@ namespace Auction.BusinessLogic.Services
         {
             TradingLot lot = FindTradingLotById(lotId);
 
-            lot.LotStatus = DataAccess.Entities.Enums.LotStatus.Verified;
+            lot.LotStatus = LotStatus.Verified;
 
             Database.TradingLots.UpdateTradingLot(lot);
             Database.Save();
         }
 
-        //add filtering
+        //add ordering
         public IEnumerable<TradingLotDTO> GetLotsForPage(int pageNum, int pageSize, int? categoryId,
             double? minPrice, double? maxPrice, string lotName, out int pagesCount, out int totalItemsCount)
         {
-            var source = FilterLots(categoryId, minPrice, maxPrice, lotName);
+            IQueryable<TradingLot> source = FilterLots(categoryId, minPrice, maxPrice, lotName);
 
             totalItemsCount = source.Count();
             if (totalItemsCount < 1)
@@ -125,7 +136,7 @@ namespace Auction.BusinessLogic.Services
             //think how to use Expression<Func<T,bool>> predicate or use query extension methods
             IQueryable<TradingLot> source = Database.TradingLots.FindTradingLots();
 
-            if (categoryId.HasValue)
+            if (categoryId.HasValue && CategoryService.IsCategoryExist(categoryId.Value))
                 source = source.Where(l => l.CategoryId == categoryId);
             if (maxPrice.HasValue)
                 if (minPrice < maxPrice)
@@ -142,8 +153,17 @@ namespace Auction.BusinessLogic.Services
 
         private TradingLot FindTradingLotById(int id)
         {
-            return Database.TradingLots.GetTradingLotById(id)
-                ?? throw new NotFoundException($"Lot with id: {id}");
+            TradingLot lotPoco;
+            try
+            {
+                lotPoco = Database.TradingLots.GetTradingLotById(id)
+                    ?? throw new NotFoundException($"Lot with id: {id}");
+            }
+            catch (InvalidOperationException)
+            {
+                throw new DatabaseException();
+            }
+            return lotPoco;
         }
     }
 }
