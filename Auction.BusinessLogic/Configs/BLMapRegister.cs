db4fd40de91c764fb6f78f2ef542b304cfe81cdf
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-08-13 20:05:17 +0300

    Config for registration new user

--- Auction.BusinessLogic/Configs/BLMapRegister.cs
+++ Auction.BusinessLogic/Configs/BLMapRegister.cs
@@ -1,6 +1,8 @@
 ﻿using Auction.BusinessLogic.DataTransfer;
 using Auction.DataAccess.Entities;
+using Auction.DataAccess.Identity.Entities;
 using Mapster;
+using System;
 
 namespace Auction.BusinessLogic.Configs
 {
@@ -9,14 +11,12 @@ namespace Auction.BusinessLogic.Configs
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<CategoryDTO, Category>().MaxDepth(3).IgnoreNullValues(true).TwoWays();
-            //maybe check configs for other entities
+
             config.NewConfig<TradeDTO, Trade>()
                 .Ignore(d => d.Id)
                 .Map(dest => dest.LotId, src => src.TradingLot.Id);
 
             config.NewConfig<Trade, TradeDTO>().MaxDepth(3);
-            //.IgnoreIf((src, dest) => !string.IsNullOrEmpty(src.LastRateUserId), dest => dest.LastRateUserId);
-            //.Ignore(dest => dest.LastRateUserId);
 
             config.NewConfig<TradingLotDTO, TradingLot>()
                 .Map(d => d.Description, s => s.Description)
@@ -26,16 +26,20 @@ namespace Auction.BusinessLogic.Configs
                 .Map(d => d.TradeDuration, s => s.TradeDuration)
                 .IgnoreNonMapped(true)
                 .IgnoreNullValues(true);
-                //.MaxDepth(4).IgnoreNullValues(true);
 
             config.NewConfig<TradingLot, TradingLotDTO>()
                 .Map(dest => dest.Status, src => src.LotStatus);
-                //.MaxDepth(5).IgnoreNullValues(true);
 
             config.NewConfig<UserDTO, UserProfile>()
                 .Ignore(dest => dest.Id);
 
             config.NewConfig<UserProfile, UserDTO>();
+
+            config.NewConfig<UserDTO, AppUser>()
+                .Map(d => d.Email, s => s.Email)
+                .Map(d => d.UserName, s => s.UserName)
+                .Map(d => d.Id, s => Guid.NewGuid().ToString())
+                .IgnoreNonMapped(true);
         }
     }
 }
