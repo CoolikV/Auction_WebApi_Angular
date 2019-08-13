f1e938b55d0ee4add5bd80f90c7068ac33418bd2
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-08-13 20:06:35 +0300

    Refactoring

--- Auction.BusinessLogic/Interfaces/IUserManager.cs
+++ Auction.BusinessLogic/Interfaces/IUserManager.cs
@@ -10,7 +10,7 @@ namespace Auction.BusinessLogic.Interfaces
     public interface IUserManager : IDisposable
     {
         Task<OperationDetails> CreateUserAsync(UserDTO userDto);
-        IEnumerable<UserDTO> GetUsersForPage(int pageNum, int pageSize, string name, out int pagesCount, out int totalItemsCount);
+        IEnumerable<UserDTO> GetUsersForPage(int pageNum, int pageSize, string userName, out int pagesCount, out int totalItemsCount);
         Task<ClaimsIdentity> Authenticate(string username, string password);
         UserDTO GetUserByUserName(string name);
         UserDTO GetUserProfileByUserName(string userName);
