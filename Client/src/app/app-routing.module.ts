02a74bf6b8ce5cc490303fc672aaf2726d0761fe
Vadim Kulik <VadimO.Kulik@livenau.net>
2019-08-21 10:47:27 +0300

    New route added

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "newLot", component: NewLotComponent },
+  { path: "/profile/trades", component: TradesListComponent },
 
   { path: "**", component: TradesListComponent }
 ];
